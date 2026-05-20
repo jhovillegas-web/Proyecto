7842ac5f3dbe3596f25681045416f7f1e382d1c3
Jhon <jho.villegas@duocuc.cl>
2026-05-20 02:42:56 -0400

    Correcion url e insercion de datos

---
+++ Sales/src/main/resources/db/migration/V1.0.2__init.sql
@@ -0,0 +1,11 @@
+INSERT INTO sales (id_client, id_store, total) VALUES
+    (1, 1, 499990),    -- Refrigerador
+    (2, 4, 999990),    -- MacBook Air M2
+    (3, 8, 69990),     -- Adidas
+    (4, 9, 89990),     -- Nike
+    (5, 6, 39990),     -- Jeans
+    (6, 11, 49990),    -- Mouse gamer
+    (7, 5, 15990),     -- Peluche
+    (8, 1, 549990),    -- PS5
+    (9, 4, 1099990),   -- iPhone 14
+    (10, 3, 69990);    -- Producto disponible en Ripley
